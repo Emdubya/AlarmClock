5159e00300f160ad1ea29bbc52c4eb42b0a7b962
Emdubya <matthewalker911@hotmail.com>
2015-03-08 09:25:28 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ <head>
+
+<?php
+ header('Cache-Control: no-cache');
+ header('Pragma: no-cache');
+ ?>
+ <link rel="shortcut icon" href="/img/favicon.ico" />
+ <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8” />
+   <title><?php echo $PageTitle; ?></title>
+ <link rel="stylesheet" href="raspberry.css">
+</head>
+<body>
